fe2352a89b669070178d87338544b17cb5daedc0
Fernandez Jose Agustin <jose.fernandez@alu.frt.utn.edu.ar>
2025-05-02 15:19:27 -0300

    Creacion clase hijas y refactorizacion

---
+++ Models/CuentaBancaria.cs
@@ -0,0 +1,52 @@
+﻿using Dsw2025Ej8.Domain;
+using Dsw2025Ej8.Exceptions;
+
+namespace Dsw2025Ej8.Models;
+
+public class CuentaBancaria
+{
+    protected TipoCuenta _tipo { get; }
+    public string _numero { get; }
+    public decimal _saldo { get; set; }
+    protected Estado _estado { get; set; }
+
+    public CuentaBancaria(string numero, decimal saldo)
+    {
+        _numero = numero;
+        _saldo = saldo;
+        _estado = Estado.Activa;
+
+    }
+
+    public virtual void Depositar(decimal monto)
+    {
+        if (_estado != Estado.Activa)
+        {
+            throw new CuentaNoActiva("No se puede operar con la cuenta {_estado})");
+        }
+
+        if (monto <= 0)
+        {
+            throw new MontoNoValido("El monto ingresado no es válido para la operación solicitada");
+        }
+    }
+
+    public virtual void Retirar(decimal monto)
+    {
+        if (_estado != Estado.Activa)
+        {
+            throw new CuentaNoActiva("No se puede operar con la cuenta {_estado})");
+        }
+        if (monto <= 0)
+        {
+            throw new MontoNoValido("El monto ingresado no es válido para la operación solicitada");
+        }
+    }
+
+}
+
+
+
+
+
+
