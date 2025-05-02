f00c5573449d7d50db3e15dad17cfd99d7d5a01a
unknown <enrique.carabajal@alu.frt.utn.edu.ar>
2025-05-02 19:41:43 -0300

    excepciones y clasesHijas

---
+++ Exceptions/Excepciones.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Exceptions
+{
+    public class MontoNoValido : Exception
+    {
+        public MontoNoValido(string mensaje) : base(mensaje) { }
+    }
+
+    public class CuentaNoActiva : Exception
+    {
+        public CuentaNoActiva(string mensaje) : base(mensaje) { }
+    }
+
+    public class SaldoInsuficiente : Exception
+    {
+        public SaldoInsuficiente(string mensaje) : base(mensaje) { }
+    }
+}
---
+++ Models/CajaDeAhorro.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dsw2025Ej8.Domain;
+using Dsw2025Ej8.Exceptions;
+
+namespace Dsw2025Ej8.Models
+{
+    public class CajaDeAhorro : CuentaBancaria
+    {
+        public decimal _tasaDeInteres = 0.03m;
+        public CajaDeAhorro(string numero, decimal saldo)
+            : base(numero, saldo)
+        {
+        }
+
+        public override void Depositar(decimal monto)
+        {
+            base.Depositar(monto);
+            _saldo += monto;
+
+        }
+
+
+        public override void Retirar(decimal monto)
+        {
+            base.Retirar(monto);
+
+            if (_saldo >= monto)
+            {
+                _saldo -= monto;
+            }
+            else
+            {
+                _estado = Estado.Suspendida;
+                throw new SaldoInsuficiente("Saldo insuficiente. La cuenta fue suspendida.");
+            }
+        }
+
+        public virtual void AplicarInteres()
+        {
+
+            _saldo += _saldo * _tasaDeInteres;
+
+        }
+    }
+}
